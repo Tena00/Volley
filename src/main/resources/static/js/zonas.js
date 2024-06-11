0b236b59684b348708c56e8d3dddffef9d7165b5
00Manu <manu.teca@hotmail.com>
2024-06-11 19:44:12 +0200

    arreglo de pequeño error

--- src/main/resources/static/js/zonas.js
+++ src/main/resources/static/js/zonas.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function() {
     obtenerZonasCampo();
+    obtenerZonasAtaque();
 });
 
 // Función para obtener los datos de las Zonas del campo desde la API
@@ -26,6 +27,34 @@ function actualizarBotonesZonasCampo(data) {
         const boton = document.getElementById(`zonaCampo${index + 1}`);
         if (boton) {
             boton.dataset.id = zonasCampo.idZonaCampo; // Guardar el ID de la zona en el botón
+        }
+    });
+}
+
+// Función para obtener los datos de las Zonas del campo desde la API
+function obtenerZonasAtaque() {
+    fetch('http://localhost:8080/zonasAtaque')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('La respuesta de la API no fue exitosa.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Datos de las zonas del campo:', data); // Verificar los datos recibidos de la API
+            actualizarBotonesZonasAtaque(data);
+        })
+        .catch(error => {
+            console.error('Error al obtener los datos de la API:', error);
+        });
+}
+
+// Función para actualizar los botones con los nombres de las zonas del campo
+function actualizarBotonesZonasAtaque(data) {
+    data.forEach((zonasAtaque, index) => {
+        const boton = document.getElementById(`zonaCampo${index + 1}`);
+        if (boton) {
+            boton.dataset.id = zonasAtaque.idZonaAtaque; // Guardar el ID de la zona en el botón
         }
     });
 }
